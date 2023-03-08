791b4cb45823e83789d7c94356116e611826b5e4
Jesse Hoffmann <hoffmann.jesse@gmail.com>
2023-03-07 22:50:13 -0800

    Starter code for moving Formspree API call to backend

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST contact info  
+  * name
+  * email
+  * message
+*/
+router.post('/', async function(req, res, next) {
+  try {
+    const response = await fetch('https://formspree.io/mzbavqpp', {
+      method: 'POST',
+      body: req.body,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+  
+    if (!response.ok) {
+      throw new Error('Error sending contact info')
+    }
+  
+    const data = await response.json()
+  
+    res.send(data)
+  } catch (e) {
+    next(e)
+  }
+});
+
+module.exports = router;
